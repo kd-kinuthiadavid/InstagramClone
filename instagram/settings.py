e71449c2c2142b565ab0170e50c1c181eabbf3ff
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2018-07-23 00:51:45 +0300

    register bootstrap4

--- instagram/settings.py
+++ instagram/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'my_instagram',
+    'bootstrap4',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
