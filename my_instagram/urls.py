d49b6be458f6c2ce7b53e1fc747fc47783c2f726
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2018-07-23 00:59:35 +0300

    add view function for all images

--- my_instagram/urls.py
+++ my_instagram/urls.py
@@ -1,9 +1,12 @@
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 
+from my_instagram import views
 
 urlpatterns=[
-#..............
+    url(r'^$', views.all_images, name = 'welcome'),
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
