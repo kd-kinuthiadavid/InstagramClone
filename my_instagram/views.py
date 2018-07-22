541db0df10fa20174ee3ca5f40396b6ee231e244
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2018-07-23 01:01:39 +0300

    edit to add view function for all images

--- my_instagram/views.py
+++ my_instagram/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import datetime as dt
+from my_instagram.models import Image, Comment
+
 
 # Create your views here.
+
+
+def welcome(request):
+    title = 'Welcome'
+    return render(request, 'welcome.html', {'title': title})
+
+def all_images(request):
+    date = dt.date.today()
+    images = Image.get_all()
+    comments = Comment.get_comments()
+    return render(request, 'index.html', {"date": date, "images": images, "comments": comments})
