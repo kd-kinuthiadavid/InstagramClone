f399b732c965529b282de03d876f6dc2af1e73cc
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2018-07-25 17:46:34 +0300

    add form for adding a New Profile

--- my_instagram/forms.py
+++ my_instagram/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Image
+from .models import Image, Profile
+
 
 class NewImageForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['user', 'post_date', 'liker']
+
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user', 'followers', 'following']
