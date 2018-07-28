181926488c4d178ab2c6ddb7d79f2a9019ba7707
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2018-07-28 18:00:50 +0300

    edit to add url for comments

--- my_instagram/urls.py
+++ my_instagram/urls.py
@@ -6,10 +6,15 @@ from my_instagram import views
 
 urlpatterns=[
     url(r'^$', views.all_images, name='welcome'),
-    url(r'^what_profile/(?P<profile_id>\d+)', views.my_profile, name='profile'),
+    url(r'^current_user_profile/(?P<profile_id>\d+)', views.my_profile, name='profile'),
     url(r'^explore_more/', views.explore, name='my_explore'),
     url(r'^new/image$', views.new_image, name='new-image'),
     url(r'^new/profile$', views.new_profile, name='new-profile'),
+    url(r'follow/(?P<id>\d+)', views.follow, name='follow'),
+    url(r'all_profiles/', views.all_profiles, name='all_profiles'),
+    url(r'single_profile/(?P<id>\d+)', views.single_profile, name='single_profile'),
+    url(r'comment/(?P<image_id>\d+)', views.comment, name='comment'),
+    url(r'comment_per-image/(?P<id>\d+)', views.comment_per_image, name='comment_per_image'),
 
 ]
 if settings.DEBUG:
